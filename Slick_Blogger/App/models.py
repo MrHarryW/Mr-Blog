a827b16bf98d9cafb37734960982774edb2a3ae9
HyScript7 <hyscript7@seznam.cz>
2025-02-05 20:23:32 +0100

    Define Post model

--- Slick_Blogger/App/models.py
+++ Slick_Blogger/App/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    # Maps to django user
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+    content = models.TextField()
+    summary = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
