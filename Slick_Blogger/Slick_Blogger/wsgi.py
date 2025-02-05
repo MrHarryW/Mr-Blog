fb751c1a58ff62920635bb601526d3f4f6ad6b00
HyScript7 <hyscript7@seznam.cz>
2025-02-05 19:42:50 +0100

    Initial upload of non-tracked code Co-Authored-By: SuperMutec <supermutec@gmail.com>

---
+++ Slick_Blogger/Slick_Blogger/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Slick_Blogger project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Slick_Blogger.settings')
+
+application = get_wsgi_application()
